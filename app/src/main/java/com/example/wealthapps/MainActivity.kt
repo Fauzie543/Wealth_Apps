b465de8fb02fccad2c2cce3f9988855b1e269927
fauzie543 <fauziekurniawan0@gmail.com>
2023-09-14 15:14:49 +0700

    pertemuan 2

--- app/src/main/java/com/example/wealthapps/MainActivity.kt
+++ app/src/main/java/com/example/wealthapps/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.wealthapps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.wealthapps.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        val name = intent.getStringExtra("name")
+//        binding.textView.text = name
     }
 }
--- app/src/main/java/com/example/wealthapps/ui/RegisterPage.kt
+++ app/src/main/java/com/example/wealthapps/ui/RegisterPage.kt
@@ -1,12 +1,27 @@
 package com.example.wealthapps.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.wealthapps.MainActivity
 import com.example.wealthapps.R
-
+import com.example.wealthapps.databinding.ActivityRegisterPageBinding
 class RegisterPage : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterPageBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register_page)
+        binding = ActivityRegisterPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button1.setOnClickListener {
+
+            val data = binding.etName.text.toString()
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("name", data)
+            startActivity(intent)
+        }
     }
 }
