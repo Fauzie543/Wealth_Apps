60fc795c260ff9e54ffc94d5206a34e0cd4c88f2
fauzie543 <fauziekurniawan0@gmail.com>
2023-12-05 15:05:28 +0700

    init

--- app/src/main/java/com/example/projek/data/remote/response/EcommerceResponse.kt
+++ app/src/main/java/com/example/projek/data/remote/response/EcommerceResponse.kt
@@ -4,10 +4,12 @@ package com.example.projek.data.remote.response
 import com.google.gson.annotations.SerializedName
 
 data class EcommerceResponse(
-    @SerializedName("data")
-    val `data`: List<Data>,
-    @SerializedName("request_id")
-    val requestId: String,
-    @SerializedName("status")
-    val status: String
+    @SerializedName("limit")
+    val limit: Int,
+    @SerializedName("products")
+    val products: List<Product>,
+    @SerializedName("skip")
+    val skip: Int,
+    @SerializedName("total")
+    val total: Int
 )
